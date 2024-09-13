5240604cb96903f7ce8445c0db2aa2a0f6ca2a52
PannaCollege <chancellor@pannacollege.org>
2024-09-13 16:12:17 +0630

    updated home page

---
+++ assets/js/home.js
@@ -0,0 +1,18 @@
+'use strict';
+/**
+ * header & go-top-btn active
+ * when window scroll down to 400px
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 400) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
